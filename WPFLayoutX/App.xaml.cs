370d756534d9bde30f7cb6249be1496e39c1bd8b
Szőcs Gábor <szocsgabor@turr.hu>
2026-02-20 10:35:32 +0100

    Add project files.

---
+++ WPFLayoutX/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFLayoutX
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
